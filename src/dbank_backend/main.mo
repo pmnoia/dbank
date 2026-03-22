11b8488f3299d96ec49466199b34b0774b6a1006
Phone Maung <phmg167@gmail.com>
2026-03-22 17:02:50 +0700

    Modify frontend

--- src/dbank_backend/main.mo
+++ src/dbank_backend/main.mo
@@ -1,5 +1,46 @@
-persistent actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+persistent actor DBank {
+  var currentValue : Float = 300.0;
+  currentValue := 300.0;
+
+  let id = 12148903124780;
+
+  var startTime = Time.now();
+  startTime := Time.now();
+  Debug.print(debug_show (startTime));
+
+  public func topUp(amount : Float) {
+    currentValue += amount;
+    Debug.print(debug_show (currentValue));
+  };
+
+  public func withdraw(amount : Float) {
+    let tempValue : Float = currentValue - amount;
+
+    if (tempValue >= 0.0) {
+      currentValue -= amount;
+      Debug.print(debug_show (currentValue));
+    } else {
+      Debug.print("Sorry! You don't have enough balance to do that.");
+    };
+  };
+
+  public query func checkBalance() : async Float {
+    Debug.print(debug_show (currentValue));
+    return currentValue;
+  };
+
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+
+    let timeElapsedFloat = Float.fromInt(timeElapsedNS);
+    let timeElapsedS = timeElapsedFloat / 1000000000.0;
+    currentValue := currentValue * (1.01 ** timeElapsedS);
+
+    startTime := currentTime;
   };
 };
--- src/dbank_frontend/src/App.jsx
+++ src/dbank_frontend/src/App.jsx
@@ -1,29 +1,48 @@
-import { useState } from 'react';
-import { dbank_backend } from 'declarations/dbank_backend';
+import { useState } from "react";
+import { dbank_backend } from "declarations/dbank_backend";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  // const [greeting, setGreeting] = useState("");
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    dbank_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const name = event.target.elements.name.value;
+  //   dbank_backend.greet(name).then((greeting) => {
+  //     setGreeting(greeting);
+  //   });
+  //   return false;
+  // }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <div class="container">
+        <img src="dbank_coin_logo.png" alt="DBank logo" width="200" />
+        <h1>
+          Current Balance: $<span id="value">300.00</span>
+        </h1>
+        <div class="divider"></div>
+        <form action="#">
+          <h2>Amount to Top Up</h2>
+          <input
+            id="input-amount"
+            type="number"
+            step="0.01"
+            min={0}
+            name="topUp"
+            value=""
+          />
+          <h2>Amount to Withdraw</h2>
+          <input
+            id="withdrawal-amount"
+            type="number"
+            name="withdraw"
+            step="0.01"
+            min={0}
+            value=""
+          />
+          <input id="submit-btn" type="submit" value="Finalise Transaction" />
+        </form>
+      </div>
     </main>
   );
 }
